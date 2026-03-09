45753c146218feb662a67390050d0ebf12b42fe7
Aatricks <melis.emilio1@gmail.com>
2026-03-10 00:29:00 +0100

    refactor: update deprecated typealiases to point to new implementations in image.diffusion

--- llmedge/src/main/java/io/aatricks/llmedge/Deprecated.kt
+++ llmedge/src/main/java/io/aatricks/llmedge/Deprecated.kt
@@ -2,6 +2,7 @@
 package io.aatricks.llmedge
 
 import io.aatricks.llmedge.text.runtime.SmolLM as SmolLMImpl
+import io.aatricks.llmedge.text.runtime.SmolLMJavaCompat as SmolLMJavaCompatImpl
 import io.aatricks.llmedge.image.diffusion.StableDiffusion as StableDiffusionImpl
 import io.aatricks.llmedge.speech.stt.Whisper as WhisperImpl
 import io.aatricks.llmedge.speech.tts.BarkTTS as BarkTTSImpl
@@ -9,9 +10,20 @@ import io.aatricks.llmedge.runtime.ModelCache as ModelCacheImpl
 import io.aatricks.llmedge.runtime.CpuTopology as CpuTopologyImpl
 import io.aatricks.llmedge.runtime.FlashAttentionHelper as FlashAttentionHelperImpl
 import io.aatricks.llmedge.runtime.GGUFReader as GGUFReaderImpl
+import io.aatricks.llmedge.image.diffusion.GenerateParams as GenerateParamsImpl
+import io.aatricks.llmedge.image.diffusion.EasyCacheParams as EasyCacheParamsImpl
+import io.aatricks.llmedge.image.diffusion.SampleMethod as SampleMethodImpl
+import io.aatricks.llmedge.image.diffusion.Scheduler as SchedulerImpl
+import io.aatricks.llmedge.image.diffusion.VideoGenerateParams as VideoGenerateParamsImpl
+import io.aatricks.llmedge.image.diffusion.LoraApplyMode as LoraApplyModeImpl
+import io.aatricks.llmedge.image.diffusion.GenerationMetrics as GenerationMetricsImpl
+import io.aatricks.llmedge.image.diffusion.PrecomputedCondition as PrecomputedConditionImpl
 
 @Deprecated("Moved to io.aatricks.llmedge.text.runtime.SmolLM", ReplaceWith("SmolLMImpl"))
 typealias SmolLM = SmolLMImpl
+
+@Deprecated("Moved to io.aatricks.llmedge.text.runtime.SmolLMJavaCompat", ReplaceWith("SmolLMJavaCompatImpl"))
+typealias SmolLMJavaCompat = SmolLMJavaCompatImpl
 
 @Deprecated("Moved to io.aatricks.llmedge.image.diffusion.StableDiffusion", ReplaceWith("StableDiffusionImpl"))
 typealias StableDiffusion = StableDiffusionImpl
@@ -33,3 +45,27 @@ typealias FlashAttentionHelper = FlashAttentionHelperImpl
 
 @Deprecated("Moved to io.aatricks.llmedge.runtime.GGUFReader", ReplaceWith("GGUFReaderImpl"))
 typealias GGUFReader = GGUFReaderImpl
+
+@Deprecated("Moved to io.aatricks.llmedge.image.diffusion", ReplaceWith("GenerateParamsImpl"))
+typealias GenerateParams = GenerateParamsImpl
+
+@Deprecated("Moved to io.aatricks.llmedge.image.diffusion", ReplaceWith("EasyCacheParamsImpl"))
+typealias EasyCacheParams = EasyCacheParamsImpl
+
+@Deprecated("Moved to io.aatricks.llmedge.image.diffusion", ReplaceWith("SampleMethodImpl"))
+typealias SampleMethod = SampleMethodImpl
+
+@Deprecated("Moved to io.aatricks.llmedge.image.diffusion", ReplaceWith("SchedulerImpl"))
+typealias Scheduler = SchedulerImpl
+
+@Deprecated("Moved to io.aatricks.llmedge.image.diffusion", ReplaceWith("VideoGenerateParamsImpl"))
+typealias VideoGenerateParams = VideoGenerateParamsImpl
+
+@Deprecated("Moved to io.aatricks.llmedge.image.diffusion", ReplaceWith("LoraApplyModeImpl"))
+typealias LoraApplyMode = LoraApplyModeImpl
+
+@Deprecated("Moved to io.aatricks.llmedge.image.diffusion", ReplaceWith("GenerationMetricsImpl"))
+typealias GenerationMetrics = GenerationMetricsImpl
+
+@Deprecated("Moved to io.aatricks.llmedge.image.diffusion", ReplaceWith("PrecomputedConditionImpl"))
+typealias PrecomputedCondition = PrecomputedConditionImpl
